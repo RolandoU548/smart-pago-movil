a05da03addf5575fc4272c321afffbec659a7160
RolandoU548 <rolandou548@gmail.com>
2024-02-21 01:01:59 -0400

    navbar responsive

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let landing_page_header = document.querySelector(".landing_page_header");
+let toggle_button_navbar = document.querySelector(".toggle_button_navbar");
+let toggle_button = document.querySelector(".toggle_button");
+let toggle_layer = document.querySelector(".toggle_layer");
+
+const toggleNavbar = () => {
+  landing_page_header.classList.toggle("landing_page_header-show");
+  toggle_layer.classList.toggle("toggle_layer-show");
+};
+
+toggle_button_navbar.addEventListener("click", toggleNavbar);
+toggle_button.addEventListener("click", toggleNavbar);
+toggle_layer.addEventListener("click", toggleNavbar);
