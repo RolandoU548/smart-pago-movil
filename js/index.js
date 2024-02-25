32b9dd83b0dc8b8640abbbb3e15ebea9e31cd8eb
RolandoU548 <rolandou548@gmail.com>
2024-02-25 01:44:13 -0400

    form

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-import { showToast } from "./utils.js";
-
 let landing_page_header = document.querySelector(".landing_page_header");
 let toggle_button_navbar = document.querySelector(".toggle_button_navbar");
 let toggle_button = document.querySelector(".toggle_button");
@@ -8,6 +6,7 @@ let toggle_layer = document.querySelector(".toggle_layer");
 const toggleNavbar = () => {
   landing_page_header.classList.toggle("landing_page_header-show");
   toggle_layer.classList.toggle("toggle_layer-show");
+  console.log("fino");
 };
 toggle_button_navbar.addEventListener("click", toggleNavbar);
 toggle_button.addEventListener("click", toggleNavbar);
