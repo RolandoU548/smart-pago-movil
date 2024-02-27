a978d5c723f77b1d7587a8e9ccce7bc83ac11cd5
RolandoU548 <rolandou548@gmail.com>
2024-02-26 22:03:02 -0400

    getActualUser

--- js/private.js
+++ js/private.js
@@ -1,14 +1,13 @@
 import { logOut, showToast, checkLogIn, getActualUser } from "./utils.js";
 checkLogIn();
+const user = getActualUser();
+
 if (localStorage.getItem("loggedFirstTime") == "true") {
   showToast("Sesión Iniciada", "check");
   localStorage.setItem("loggedFirstTime", "false");
 }
 let logout__button = document.querySelector(".logout__button");
 logout__button.addEventListener("click", logOut);
-
-const user = getActualUser();
-console.log(user.amount);
 
 const divEmail = document.querySelector(".div__email");
 const divAmount = document.querySelector(".div__amount");
