7b38c8e3f42b64cacb34be355c17ddd98219f2df
RolandoU548 <rolandou548@gmail.com>
2024-03-03 01:41:43 -0400

    css private

--- js/private.js
+++ js/private.js
@@ -14,3 +14,12 @@ const divAmount = document.querySelector(".div__amount");
 
 divEmail.innerHTML = `USUARIO: <span>${user.email}</span>`;
 divAmount.innerHTML = `SALDO DISPONIBLE: <span>${user.amount}bs</span>`;
+
+const options = document.querySelectorAll(".options");
+options[0].addEventListener("click", () => {
+  window.location.href = "./manual.html";
+});
+options[1].addEventListener("click", () => {
+  window.location.href = "./qr.html";
+});
+console.log(options);
