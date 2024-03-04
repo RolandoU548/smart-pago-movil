8f8185afb50176539dfcf18ba7d327c6c40b6be0
RolandoU548 <rolandou548@gmail.com>
2024-03-04 03:15:53 -0400

    chat

--- js/chat_pago.js
+++ js/chat_pago.js
@@ -3,10 +3,32 @@ usuario.nombre;
 
 const keyboardForm = document.querySelector(".keyboard__form");
 const keyboard = document.querySelector(".keyboard");
-const chat = document.querySelector("chat");
+const messageContainer = document.querySelector(".message__container");
+const chat = document.querySelector(".chat");
+
+const sendMessage = (message) => {
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
+  messageDiv.classList.add("outcoming");
+  messageDiv.innerHTML = message;
+  messageContainer.appendChild(messageDiv);
+  chat.scrollTop = chat.scrollHeight;
+};
+
+const receiveMessage = (message) => {
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
+  messageDiv.classList.add("incoming");
+  messageDiv.innerHTML = message;
+  messageContainer.appendChild(messageDiv);
+  chat.scrollTop = chat.scrollHeight;
+};
 
 const handleSubmit = (e) => {
   e.preventDefault();
+  sendMessage(keyboard.value);
+  receiveMessage("OK");
+  keyboardForm.reset();
 };
 
 keyboardForm.addEventListener("submit", handleSubmit);
