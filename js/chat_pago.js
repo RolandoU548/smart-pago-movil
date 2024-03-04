fc607bc6e102232fd8e6e95e9efcb058bbdb0f89
RolandoU548 <rolandou548@gmail.com>
2024-03-04 09:33:41 -0400

    pagar por sms

--- js/chat_pago.js
+++ js/chat_pago.js
@@ -1,3 +1,5 @@
+import { generateOperationCode } from "./utils.js";
+
 const usuario = { nombre: "juan", apellido: "beli" };
 usuario.nombre;
 
@@ -5,6 +7,8 @@ const keyboardForm = document.querySelector(".keyboard__form");
 const keyboard = document.querySelector(".keyboard");
 const messageContainer = document.querySelector(".message__container");
 const chat = document.querySelector(".chat");
+const messageRegex = /^Pagar \d{4} \d{11} \d+ \d+$/;
+const phoneRegex = /^04(12|14|16|24|26)\d{7}$/;
 
 const sendMessage = (message) => {
   const messageDiv = document.createElement("div");
@@ -26,12 +30,40 @@ const receiveMessage = (message) => {
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  if (keyboard.value == "") {
+  const userMessage = keyboard.value;
+  keyboard.value = "";
+  if (userMessage == "") {
     return false;
   }
-  sendMessage(keyboard.value);
-  receiveMessage("OK");
-  keyboard.value = "";
+  sendMessage(userMessage);
+  if (!messageRegex.test(userMessage)) {
+    receiveMessage(
+      `El mensaje debe tener la siguiente estructura: "Pagar &lt;código de Banco del destinatario&gt;	&lt;número de teléfono del destinatario&gt; &lt;cédula del destinatario&gt; &lt;monto&gt;`
+    );
+    return false;
+  }
+  const userMessages = userMessage.split(" ");
+  if (!phoneRegex.test(userMessages[2])) {
+    receiveMessage("Télefono Inválido");
+    return false;
+  }
+  if (userMessages[3] < 1 || userMessages[3] > 33000000) {
+    receiveMessage("Cédula Inválida");
+    return false;
+  }
+  if (userMessages[4] < 1) {
+    receiveMessage("Monto Inválido");
+    return false;
+  }
+  if (userMessages[4] > 1000000) {
+    receiveMessage("El monto máximo es de un millón");
+    return false;
+  }
+  receiveMessage(
+    `Pago realizado con éxito. Código de Operación:${generateOperationCode()}. Monto:${
+      userMessages[4]
+    },00bs`
+  );
 };
 
 keyboardForm.addEventListener("submit", handleSubmit);
