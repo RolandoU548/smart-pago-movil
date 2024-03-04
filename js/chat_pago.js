56273e250049e648757fadd9d9e65fb30c990fe4
RolandoU548 <rolandou548@gmail.com>
2024-03-04 02:39:44 -0400

    menos padding

--- js/chat_pago.js
+++ js/chat_pago.js
@@ -1,3 +1,12 @@
 const usuario = { nombre: "juan", apellido: "beli" };
 usuario.nombre;
-// alert("tu codigo es 1234");
+
+const keyboardForm = document.querySelector(".keyboard__form");
+const keyboard = document.querySelector(".keyboard");
+const chat = document.querySelector("chat");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
+
+keyboardForm.addEventListener("submit", handleSubmit);
