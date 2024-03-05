66a508fcf2a2fae943cf40085825b7686f331aff
RolandoU548 <rolandou548@gmail.com>
2024-03-05 01:56:22 -0400

    sms terminado

--- js/sms.js
+++ js/sms.js
@@ -4,3 +4,22 @@ if (
 ) {
   localStorage.setItem("amountSMS", 500);
 }
+
+const months = [
+  "ene",
+  "feb",
+  "mar",
+  "jun",
+  "jul",
+  "ago",
+  "sep",
+  "oct",
+  "nov",
+  "dic",
+];
+
+const today = new Date(Date.now());
+const dates = document.querySelectorAll(".actualDate");
+dates.forEach((date) => {
+  date.innerHTML = `${today.getDate()} ${months[today.getMonth()]}.`;
+});
